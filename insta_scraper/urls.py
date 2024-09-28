7803895240dc94e823fa9dacaddf26be7dd5c0df
plss12 <pepoluis712@gmail.com>
2024-09-28 21:19:44 +0200

    Añadida pantalla de querys para navegar por los datos scrapeados

--- insta_scraper/urls.py
+++ insta_scraper/urls.py
@@ -5,9 +5,9 @@ from scraper import views
 urlpatterns = [
     path('',views.index),
     path('ad/',views.admin_win),
-    path('db',views.database),
-    path('profile',views.profile),
-    path('scrap',views.scrap),
-    path('scraping_admin',views.scraping_admin),
+    path('db/',views.database),
+    path('profile/',views.profile),
+    path('scrap/',views.scrap),
+    path('scraping_admin/',views.scraping_admin),
     path('admin/', admin.site.urls),
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -47,25 +47,30 @@ def profile(request):
     if request.method == 'POST':
         profile = request.POST.get('usuario')
         data = Profile.objects.get(profile=profile)
+        request.session['usuario'] = profile 
         return render(request, 'Profile.html', {'usuario': data})   
 
     if request.method == 'GET':
         profile = request.session.get('usuario', None)
         data_type = request.GET.get('type')
+        html = ""
+
         if data_type == 'both':
             text = "Mutuos"
-            mutual = Insta.objects.get(profile=profile, followers=True, following=True)
+            mutual = Insta.objects.filter(profile=profile, following=1, follower=1)
             context = {'data': mutual, 'text': text}
             html = render_to_string('Table.html', context)
+
         elif data_type == 'followers':
             text = "Seguidores no seguidos"
-            followers = Insta.objects.get(profile=profile, followers=True, following=False)
-            context = {'data': followers}
+            followers = Insta.objects.filter(profile=profile, following=0, follower=1)
+            context = {'data': followers, 'text': text}
             html = render_to_string('Table.html', context)
+
         elif data_type == 'following':
             text = "Seguidos no seguidores"
-            following = Insta.objects.get(profile=profile, following=True, followers=False)
-            context = {'data': following}
+            following = Insta.objects.filter(profile=profile, following=1, follower=0)
+            context = {'data': following, 'text': text}
             html = render_to_string('Table.html', context)
 
         return JsonResponse({'html': html})
